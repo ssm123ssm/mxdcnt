84329f6b1d10379dfdd184c50b3ed0fe6087d88c
ssm123ssm <ssm123ssm@gmail.com>
2019-06-22 14:14:23 +0530

    df

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var axios = require('axios');
+var cors = require('cors');
 var port = 80;
 
 var app = express();
+app.use(cors());
+app.use(bodyParser.json({
+    limit: '50mb'
+})); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+    limit: '50mb',
+    extended: true
+}));
 
 app.listen(process.env.PORT || port, function () {
     console.log('Started');
 });
 
 app.get('/', function (req, res) {
-    res.send('Still?');
+    res.send('Works...');
+});
+
+app.post('/post', function (req, res) {
+    var url = req.body.url;
+    var data = req.body;
+    console.log(url, data);
+    axios.post(url, data).then(function (response) {
+        console.log(response.data);
+        res.send(response.data);
+    }).catch(function (error) {
+        console.log(error);
+        res.send('error');
+    })
+});
+
+app.get('/get', function (req, res) {
+    var url = req.params.url;
+    console.log(url);
+    axios.get(url).then(function (response) {
+        console.log(response.data);
+        res.send(response.data);
+    }).catch(function (error) {
+        console.log(error);
+        res.send('error');
+    })
 });
