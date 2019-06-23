864679278357bc67fbb741e627fdcaadfc3ed860
ssm123ssm <ssm123ssm@gmail.com>
2019-06-23 15:42:04 +0530

    df

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var axios = require('axios');
 var cors = require('cors');
+var fs = require('fs');
 var port = 80;
 
 var app = express();
@@ -43,16 +44,32 @@ app.get('/get', function (req, res) {
     if (ext == 'css') {
         res.type('text/css');
     }
-    var params = req.query;
-    //params.delete['url'];
-    console.log('Getting the Address...');
-    axios.get(url, {
-        params: params
-    }).then(function (response) {
-        console.log(response.data);
-        res.send(response.data);
-    }).catch(function (error) {
-        console.log('error');
-        res.send('error');
-    })
+    if (ext == 'jpg' || ext == 'jpeg' || ext == 'png') {
+        axios({
+                method: 'get',
+                url: url,
+                responseType: 'stream'
+            })
+            .then(function (response) {
+                var fileName = `img.${ext}`;
+                var st = response.data.pipe(fs.createWriteStream(fileName));
+                st.on('finish', function () {
+                    res.sendFile(__dirname + `/${fileName}`);
+                })
+            });
+    } else {
+        var params = req.query;
+        //params.delete['url'];
+        console.log('Getting the Address...');
+        console.log(url);
+        axios.get(url, {
+            params: params
+        }).then(function (response) {
+
+            res.send(response.data);
+        }).catch(function (error) {
+            console.log('error');
+            res.send('error');
+        })
+    }
 });
